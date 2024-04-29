6f3298373fdaa1b7d6c234d73adc50a902a14181
Nick Anderson Azémar <76443393+nico-arch@users.noreply.github.com>
2024-04-29 18:48:31 +0000

    Set a fetched list of users

---
+++ src/components/UsersList.jsx
@@ -0,0 +1,74 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Card, Col, Container, Row } from "react-bootstrap";
+//import process from "dotenv";
+
+const UsersList = () => {
+  //const API_URL_V1 = process.env.API_URL_V1;
+
+  const getAllUsers = `${API_URL_V1}/all`;
+
+
+  const [users, setUsers] = useState({});
+
+  const fetchUsers = async () => {
+    try {
+      const res = await axios.get(getAllUsers);
+      console.log("Fetched data:", res.data.users); // Log fetched data
+      setUsers(res.data.users);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  //console.log(users);
+  /*const renderedUsers = Object.values(users).map((user, index) => {
+    return (
+      <React.Fragment key={user.id}>
+        <Row className="justify-content-center">
+          <Col lg={4}>
+            <Card>
+              <Card.Body>
+                <h4>{user.name}</h4>
+                <p>{user.email}</p>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </React.Fragment>
+    );
+  }); */
+
+  return (
+    <Container className="mt-5 mb-5">
+      <h3 className="text-center mb-3">Users</h3>
+      <Container>
+        {Object.values(users).map((user, index) => (
+          <React.Fragment key={user.id}>
+            <Row className="justify-content-center">
+              <Col lg={4}>
+                <Card>
+                  <Card.Body>
+                    <h4>{user.name}</h4>
+                    <p>{user.email}</p>
+                    {user.city && user.country && (
+                      <p>
+                        {user.city}, {user.country}
+                      </p>
+                    )}
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </React.Fragment>
+        ))}
+      </Container>
+    </Container>
+  );
+};
+
+export default UsersList;

1358e32deedce69e371797f9d271d966633569c5
Nick Anderson Azémar <76443393+nico-arch@users.noreply.github.com>
2024-04-29 18:46:53 +0000

    Define API path

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-  }
-})
+    host: "0.0.0.0",
+  },
+  define: {
+    API_URL_V1: `"${process.env["API_URL_V1"]}"`,
+  },
+});

1ff4f053ed7453403f900eb8b300b6f649c91d94
Nick Anderson Azémar <76443393+nico-arch@users.noreply.github.com>
2024-04-29 18:51:43 +0000

    Show the user list

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import './App.css'
+import { Container } from "react-bootstrap";
+import "./App.css";
+import UsersList from "./components/UsersList";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
   return (
     <main>
-      React ⚛️ + Vite ⚡ + Replit
+      {/* React ⚛️ + Vite ⚡ + Replit */}
+      <Container fluid className="mt-4 mb-3">
+        <UsersList />
+      </Container>
     </main>
-  )
+  );
 }

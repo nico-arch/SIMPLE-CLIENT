8f7f01ae72c66075dfa0690debfc30ff13572ba2
Nick Anderson Azémar <76443393+nico-arch@users.noreply.github.com>
2024-05-02 17:25:38 +0000

    Implemented React Router Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 import { Container } from "react-bootstrap";
 import "./App.css";
 import UsersList from "./components/UsersList";
+import CreateUser from "./components/CreateUser";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import { createGlobalStyle } from "styled-components";
+
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+const BackgroundColor = createGlobalStyle`
+body{
+      background-color: #f2f2f2;
+}
+`;
+
 export default function App() {
   return (
-    <main>
+    <>
       {/* React ⚛️ + Vite ⚡ + Replit */}
+      <BackgroundColor />
       <Container fluid className="mt-4 mb-3">
-        <UsersList />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<UsersList />} />
+            <Route path="/create" element={<CreateUser />} />
+          </Routes>
+        </BrowserRouter>
       </Container>
-    </main>
+    </>
   );
 }

1693e5d60af0a8456ff99ab990cfb5068b7eb78e
Nick Anderson Azémar <76443393+nico-arch@users.noreply.github.com>
2024-05-03 18:37:30 +0000

    Add a user retrieve component

---
+++ src/components/RetrieveUser.jsx
@@ -0,0 +1,13 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Container, Button, Form, Row, Col } from "react-bootstrap";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+
+const RetrieveUser = () => {
+  const getUserEndpoint = `${API_URL_V1}/get`;
+  
+  return <></>;
+};
+
+export default RetrieveUser;

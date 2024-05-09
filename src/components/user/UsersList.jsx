0deeacd7fed47020e5d1d5b3f8e90aeb74dcecfa
Nick Anderson Azémar <76443393+nico-arch@users.noreply.github.com>
2024-05-09 19:09:03 +0000

    Handle the Network connection and No User Error

--- src/components/user/UsersList.jsx
+++ src/components/user/UsersList.jsx
@@ -3,10 +3,12 @@ import { NavLink } from "react-router-dom";
 import { Card, Col, Container, Row, Button } from "react-bootstrap";
 import Layout from "../layout/Layout";
 import * as userService from "../../services/user.service";
+import { toast } from "react-toastify";
 //import process from "dotenv";
 
 const UsersList = () => {
   const [users, setUsers] = useState({});
+  const [errorMessage, setErrorMessage] = useState("");
 
   const fetchUsers = async () => {
     try {
@@ -14,7 +16,19 @@ const UsersList = () => {
       //console.log("Fetched data:", res.data.users); // Log fetched data
       setUsers(res.data.users);
     } catch (error) {
-      console.error("Error fetching users:", error);
+      if (error.response?.data?.status === false) {
+        //No user found message
+        const userNotFoundMessage = error.response.data.message;
+        setErrorMessage(userNotFoundMessage);
+        toast.warn(userNotFoundMessage);
+        console.error(userNotFoundMessage);
+      } else {
+        const serverIsDownMessage =
+          "The server is down, please try again later";
+        setErrorMessage(serverIsDownMessage);
+        toast.error(serverIsDownMessage);
+        console.error(error.message);
+      }
     }
   };
 
@@ -24,36 +38,45 @@ const UsersList = () => {
 
   return (
     <Layout>
-      
-      <h3 className="text-center mb-3">Users</h3>
-      <Container>
-        {Object.values(users).map((user, index) => (
-          <React.Fragment key={user.id}>
-            <Row className="justify-content-center">
-              <Col lg={4}>
-                <Card className="mt-2">
-                  <Card.Body>
-                    <h4>{user.name}</h4>
-                    <p>{user.email}</p>
-                    {user.city && user.country && (
-                      <p>
-                        {user.city}, {user.country}
-                      </p>
-                    )}
-                    <Button
-                      variant="secondary"
-                      as={NavLink}
-                      to={`edit/${user.id}`}
-                    >
-                      Edit user
-                    </Button>
-                  </Card.Body>
-                </Card>
-              </Col>
-            </Row>
-          </React.Fragment>
-        ))}
-      </Container>
+      {errorMessage ? (
+        <>
+          <h3 className="text-center text-danger fw-bold mb-3">
+            {errorMessage}
+          </h3>
+        </>
+      ) : (
+        <>
+          <h3 className="text-center mb-3">Users</h3>
+          <Container>
+            {Object.values(users).map((user, index) => (
+              <React.Fragment key={user.id}>
+                <Row className="justify-content-center">
+                  <Col lg={4}>
+                    <Card className="mt-2">
+                      <Card.Body>
+                        <h4>{user.name}</h4>
+                        <p>{user.email}</p>
+                        {user.city && user.country && (
+                          <p>
+                            {user.city}, {user.country}
+                          </p>
+                        )}
+                        <Button
+                          variant="secondary"
+                          as={NavLink}
+                          to={`edit/${user.id}`}
+                        >
+                          Edit user
+                        </Button>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                </Row>
+              </React.Fragment>
+            ))}
+          </Container>
+        </>
+      )}
     </Layout>
   );
 };
